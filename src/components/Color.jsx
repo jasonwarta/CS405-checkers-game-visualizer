bea61cb9652debffe47657019e92596779b1a7fb
Jason Warta <jasonwarta@gmail.com>
2018-03-12 12:06:37 -0800

    Updates

--- src/components/Color.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Color(props) {
-	return (
-		<div style={{backgroundColor: props.color}} {...props} ></div>
-	);
-}
--- src/static/tiles.js
+++
@@ -1,9 +0,0 @@
-const TILES = { 
-	'_': ['',''], 
-	'b': ['b','black'], 
-	'B': ['bk','black'], 
-	'r': ['r','red'], 
-	'R': ['rk','red'],
-};
-
-module.exports = TILES;
